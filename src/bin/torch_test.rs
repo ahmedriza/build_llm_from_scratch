9a466bb1e11715949f01fe24e35990dffebcbf93
Ahmed Riza <dr.riza@gmail.com>
2025-10-28 21:21:48 +0000

    Build an LLM from Scratch

--- src/bin/torch_test.rs
+++ src/bin/torch_test.rs
@@ -3,11 +3,14 @@ use tch::kind;
 
 fn main() -> anyhow::Result<()> {
     let t = Tensor::from_slice(&[1, 2, 3]);
-    let t = t * 2;
-    // t.print(); // works on cpu tensors
-
-    println!("t(Cpu): {:?}", &t);
     println!("t device: {:?}", &t.device());
+
+    let t = t * 2;
+    println!("t(Cpu): {:?}", &t);
+    let t = t / 2;
+    println!("t(Cpu): {:?}", &t);
+    let t = Tensor::exp(&t);
+    println!("t(Cpu): {:?}", &t);
     println!("");
 
     let t = Tensor::randn([5, 4], kind::FLOAT_CPU).to_device(tch::Device::Mps);
